7fab150978412bd2e794fe0d57dbc4edf348eab8
talholm-hub <58832240+talholm-hub@users.noreply.github.com>
2019-12-12 18:08:52 -0600

    TY YOU SUCK

---
+++ Project8/Board.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project8
+{
+    public enum PieceColor { empty, red, black};
+    PieceColor col = PieceColor.black;
+    public partial class Board : Form
+    {
+        public Board()
+        {
+            InitializeComponent();
+        }
+        public PieceColor Turn
+        {
+
+        }
+        public PieceColor GetColor(int x,int y)
+        {
+
+        }
+
+        public bool isWinner(PieceColor player)
+        {
+
+        }
+
+        public bool CheckTie()
+        {
+
+        }
+
+        public void SwitchTurns()
+        {
+
+        }
+
+        public bool Move(int col)
+        {
+
+        }
+
+        }
+    }
+
---
+++ Project8/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project8
+{
+    public partial class Form1 : Form
+    {
+        private PictureBox[,] _grid = new PictureBox[6, 7];
+        public Form1()
+        {
+            InitializeComponent();
+
+            int yPos = 58;
+            for (int i = 0; i < 6; i++)
+            {
+                int xPos = 13;
+                for (int j = 0; j < 7; j++)
+                {
+
+                }
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
